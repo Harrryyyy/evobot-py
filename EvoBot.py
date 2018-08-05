7cfc9e3faa6512f053281f9f218bfc31c3c0bf01
Harrryyyy <39433040+Harrryyyy@users.noreply.github.com>
2018-08-05 20:54:32 +0100

    Update EvoBot.py

--- EvoBot.py
+++ EvoBot.py
@@ -98,6 +98,7 @@ async def mute(ctx, member: discord.Member, *, reason: str):
 async def unmute(ctx, member: discord.Member):
     mute_role = discord.utils.get(member.server.roles, name='Muted')
     await client.remove_roles(member, mute_role)
+    await client.send_message(message.channel, ":white_check_mark: Member ``{member}`` successfully unmuted.")
 
 
 
