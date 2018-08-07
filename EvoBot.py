cf8f65c2cebe6cddd981cc5ec81adfb680cd59cf
Harrryyyy <39433040+Harrryyyy@users.noreply.github.com>
2018-08-07 22:30:32 +0100

    Update EvoBot.py

--- EvoBot.py
+++ EvoBot.py
@@ -12,12 +12,12 @@ Client = discord.Client()
 client = commands.Bot(command_prefix = "$")
 client.remove_command('help')
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot successfully booted & online.")
-    print("My name is " + client.user.name)
-    print("My ID is " + client.user.id)
-    await client.change_presence(game=discord.Game(type=2,name='$help'))
+    print("My name is " + bot.user.name)
+    print("My ID is " + bot.user.id)
+    await bot.change_presence(game=discord.Game(type=2,name='$help'))
 
 @bot.event
 async def on_member_join(member):
@@ -28,25 +28,25 @@ async def on_member_join(member):
         role = discord.utils.get(member.server.roles, name='Members')
         await bot.add_roles(member, role)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_role("Mod")
 async def echo(ctx, *, args):
-    await client.say(args)
-    await client.delete_message(ctx.message)
+    await bot.say(args)
+    await bot.delete_message(ctx.message)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def ping(ctx):
         '''Ping bot'''
         channel = ctx.message.channel
         t1 = time.perf_counter()
-        msg = await client.say("Pong!")
+        msg = await bot.say("Pong!")
         t2 = time.perf_counter()
-        await client.edit_message(msg, "Pong! `{}ms`".format(round((t2-t1)*1000)))
+        await bot.edit_message(msg, "Pong! `{}ms`".format(round((t2-t1)*1000)))
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=100):
-    await client.purge_from(ctx.message.channel, limit=int(amount) + 1)
+    await bot.purge_from(ctx.message.channel, limit=int(amount) + 1)
     await client.say ('**✓** | Messages cleared from chat history.')
 
 @client.command(pass_context=True)
@@ -54,28 +54,28 @@ async def clear(ctx, amount=100):
 async def rank(ctx, member: discord.Member, rank: str):
     if rank == "Developers":
         role = discord.utils.get(member.server.roles, name='Developers')
-        await client.add_roles(member, role)
-        return await client.say(f'Successfully ranked user ``{member}`` to **{role}**')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
     if rank == "Support":
         role = discord.utils.get(member.server.roles, name='Support')
-        await client.add_roles(member, role)
-        return await client.say(f'Successfully ranked user ``{member}`` to **{role}**')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
     if rank == "Coders":
         role = discord.utils.get(member.server.roles, name='Coders')
-        await client.add_roles(member, role)
-        return await client.say(f'Successfully ranked user ``{member}`` to **{role}**')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
     if rank == "Mod":
         role = discord.utils.get(member.server.roles, name='Mod')
-        await client.add_roles(member, role)
-        return await client.say(f'Successfully ranked user ``{member}`` to **{role}**')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
     if rank == "Partners":
         role = discord.utils.get(member.server.roles, name='Partners')
-        await client.add_roles(member, role)
-        return await client.say(f'Successfully ranked user ``{member}`` to **{role}**')
+        await cbot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
     if rank == "W.I.P":
         role = discord.utils.get(member.server.roles, name='W.I.P')
-        await client.add_roles(member, role)
-        return await client.say(f'**✓** | Successfully ranked user ``{member}`` to **{role}**')
+        await bot.add_roles(member, role)
+        return await bot.say(f'**✓** | Successfully ranked user ``{member}`` to **{role}**')
     
 @bot.command(pass_context=True)
 async def kick(ctx, userName: discord.Member, *, reason: str):
