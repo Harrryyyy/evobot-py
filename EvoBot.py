29850ed183aaf9f1165212f0a7b5e8a9f559fd25
Harrryyyy <39433040+Harrryyyy@users.noreply.github.com>
2018-08-08 15:51:46 +0100

    Update EvoBot.py

--- EvoBot.py
+++ EvoBot.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
+import asyncio
+import time
+import random
 
-bot = commands.Bot(command_prefix = "$")
+bot = commands.Bot(command_prefix = ";")
 bot.remove_command('help')
 
 @bot.event
@@ -9,19 +12,23 @@ async def on_ready():
     print("Bot successfully booted & online.")
     print("My name is " + bot.user.name)
     print("My ID is " + bot.user.id)
-    await bot.change_presence(game=discord.Game(type=2,name='$help'))
+    await bot.change_presence(game=discord.Game(type=2,name='EvoBot | W.I.P'))
 
-@bot.command()
-async def help():
-    embed = discord.Embed(title="Help page", description="This would be the help list", color=0x646666)
-    await bot.say(embed=embed)
+@bot.event
+async def on_member_join(member):
+    if member.server.id == "476386834054905857":
+        channel = discord.utils.get(member.server.channels, name='welcome-messages')
+        embed = discord.Embed(title=f"Welcome {member}!", description=f"to Evolutionary! Enjoy your stay.", color=0x646666)
+        await bot.send_message(channel, embed=embed)
+        role = discord.utils.get(member.server.roles, name='Members')
+        await bot.add_roles(member, role)
 
 @bot.command(pass_context=True)
 @commands.has_role("Mod")
 async def echo(ctx, *, args):
     await bot.say(args)
     await bot.delete_message(ctx.message)
-    
+
 @bot.command()
 async def ping():
         t1=time.perf_counter()
@@ -29,6 +36,36 @@ async def ping():
         t2=time.perf_counter()
         await bot.edit_message(msg, embed=discord.Embed(title='\U0001f3d3 Pong!',description=f'**{round((t2-t1)*1000)}** milliseconds',colour=discord.Colour.green()))
 
+@bot.command(pass_context=True)
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=100):
+    await bot.purge_from(ctx.message.channel, limit=int(amount) + 1)
+    await bot.say ('Messages cleared from chat history.')
+
+@bot.command(pass_context=True)
+@commands.has_role("Management")
+async def rank(ctx,member: discord.Member, rank: str):
+    if rank == "Developers":
+        role = discord.utils.get(member.server.roles, name='Developers')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
+    if rank == "Coders":
+        role = discord.utils.get(member.server.roles, name='Coders')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
+    if rank == "Mod":
+        role = discord.utils.get(member.server.roles, name='Mod')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
+    if rank == "Partners":
+        role = discord.utils.get(member.server.roles, name='Partners')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
+    if rank == "W.I.P":
+        role = discord.utils.get(member.server.roles, name='W.I.P')
+        await bot.add_roles(member, role)
+        return await bot.say(f'Successfully ranked user ``{member}`` to **{role}**')
+    
 @bot.command(pass_context=True)
 async def kick(ctx, userName: discord.Member, *, reason: str):
     if ctx.message.author.server_permissions.kick_members:
@@ -39,8 +76,8 @@ async def kick(ctx, userName: discord.Member, *, reason: str):
         await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
+@commands.has_role("Mod")
 async def ban(ctx, userName: discord.Member, *, reason: str):
-    if ctx.message.author.server_permissions.ban_members:
         embed = discord.Embed(title=f"{userName}", description=f"You have been banned from Evolutionary for {reason}.", color=0x646666)
         await bot.send_message(userName, embed=embed)
         await bot.ban(userName)
@@ -68,13 +105,6 @@ async def unmute(ctx, member: discord.Member):
         embed = discord.Embed(title=f"User {member} has successfully been unmuted.", description="", color=0x646666)
         await bot.say(embed=embed)
 
-@bot.event
-async def on_member_join(member):
-    if member.server.id == "476386834054905857":
-        channel = discord.utils.get(member.server.channels, name='welcome-messages')
-        embed = discord.Embed(title=f"Welcome {member}!", description=f"to Evolutionary! Enjoy your stay.", color=0x646666)
-        await bot.send_message(channel, embed=embed)
-        role = discord.utils.get(member.server.roles, name='Members')
-        await bot.add_roles(member, role)
-   
+
+
 bot.run("NDc1MDEyMTI3OTM0MTE5OTQ3.DkY5XA.u69rTAwBa9lwp-pw9rxigAxDF6M")
